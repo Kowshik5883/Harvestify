8dde5da670272049912ec6e89b114c49ec2efa51
Kowshik Darla <darla.kowshik@gmail.com>
2025-05-03 19:03:42 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ import io
 import torch
 from torchvision import transforms
 from PIL import Image
-from utils.model import ResNet9
+from app.utils.model import ResNet9
 
 
 # ==============================================================================================
